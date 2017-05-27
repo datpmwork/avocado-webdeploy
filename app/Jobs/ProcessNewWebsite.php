269f13c8aafedcd8dc02539d4aa4d829b8827c57
Phạm Minh Đạt <cse.pmdat@gmail.com>
2017-05-27 18:02:14 +0700

    [SYS] - Website Folder Permission

--- app/Jobs/ProcessNewWebsite.php
+++ app/Jobs/ProcessNewWebsite.php
@@ -34,32 +34,24 @@ class ProcessNewWebsite implements ShouldQueue
         $website = $this->website;
         $password = env('GLOBAL_PASSWD', '');
         # Create Website User in System
-        shell_exec("sshpass -p '{$password}' sudo useradd -p `mkpasswd \"{$website->password}\"` -d /home/\"{$website->username}\" -m -g users -s /bin/bash \"{$website->username}\"");
+        shell_exec("sshpass -p '{$password}' sudo useradd -p `mkpasswd \"{$website->password}\"` -d /home/\"{$website->username}\" -m -s /bin/bash \"{$website->username}\"");
 
         # Create Directory to Store Deploy source and Version Control
         $website->document_root = "/home/{$website->username}/deploy";
         $website->git_root = "/home/{$website->username}/{$website->username}.git";
-        \File::makeDirectory($website->git_root, 770, false, true);
-        \File::makeDirectory($website->document_root, 770, false, true);
+        \File::makeDirectory($website->git_root, 770);
+        \File::makeDirectory($website->document_root, 770);
 
         # Git Init Bare
-        exec("cd {$website->git_root}; git init --bare;");
+        shell_exec("git init --bare {$website->git_root}");
 
         # Create Deploy Code
         $deploy_path = "{$website->git_root}/hooks/post-receive";
         \File::put($deploy_path, view('scripts.post-receive', compact('website')));
 
         # Change Folder Permission
-        chownr($website->document_root, $website->username);
-        chownr($website->git_root, $website->username);
-        chownr($deploy_path, $website->username);
-
-        chgrpr($website->document_root, $website->username);
-        chgrpr($website->git_root, $website->username);
-        chgrpr($deploy_path, $website->username);
-
-        chmodr($website->document_root, "0770");
-        chmodr($website->git_root, "0770");
-        chmodr($deploy_path, "0770");
+        chmodr("/home/{$website->username}/", 0760);
+        chgrpr("/home/{$website->username}/", $website->username);
+        chownr("/home/{$website->username}/", $website->username);
     }
 }
